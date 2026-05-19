9fe4f48652667bf1296f00eb9329dac4d1f07375
DianaLeeCelestra <dianalee030@gmail.com>
2026-05-19 12:53:12 +0800

    Update types.ts

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,5 +1,5 @@
 import type { SingleValue, MultiValue } from "react-select";
-import type { Options } from "../components/appointmentTable/allAppointments/table";
+import type { Options } from "../components/patient/appointmentTable/allAppointments/table";
 
 export type FilterValue = SingleValue<Options> | MultiValue<Options> | null;
 export type FiltersState = Record<string, FilterValue>;
